5e624231df2ee66da57999b33be9832578060273
Erik Holden <erikholden4@gmail.com>
2023-10-04 10:33:31 -0400

    ShoppingList

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main (String[] args){
+        String groceryList = "Milk, Bread, Tomatoes, Beef, Peanuts, Watermelons, Onions, Carrots, Fries, Mayo";
+        System.out.println("Shopping List: " + groceryList);
+
+    }
+
+}
